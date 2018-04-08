8e9c9610bd62d7d2d5806d906ad70c05f351e5f1
Magdalene Lee <magdalene.lee@bookspan.com>
2018-04-08 00:33:47 -0700

    add some flexbox styles

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import './BookCard.css'
 const BookCard = ({book}) => {
   return (
-    <div>
-      { book.name }
+    <div className="flex-item">
+      <span className="Title-Box"> { book.name } </span>
+      <img className="Book-Cover" src={book['cover-img']} />
     </div>
   )
 }
