d2cc778681ed5992cbea5d1602ca44249c4b9123
magdalene lee <me@magdalenelee.com>
2018-04-09 16:19:50 -0700

    add text filtering basic

--- src/containers/DisplayOptions/DisplayOptions.js
+++ src/containers/DisplayOptions/DisplayOptions.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import './DisplayOptions.css'
+import React from "react";
+import "./DisplayOptions.css";
 
-class DisplayOptions extends React.Component {
-  render() {
-    return (
-      <div className="Display-Options">
-        <input type="text" className="PushLeft"/>
-        <div className="PushRight"> &nbsp; </div>
-        <div className="Options-Icons-Container">
-          <span> square</span> <span> hambu </span>
-        </div>
+const DisplayOptions = ({ setSearchFilter }) => {
+  return (
+    <div className="Display-Options">
+      <input
+        type="text"
+        className="PushLeft"
+        onChange={event => setSearchFilter(event.target.value)}
+      />
+      <div className="PushRight"> &nbsp; </div>
+      <div className="Options-Icons-Container">
+        <span> square</span> <span> hambu </span>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default DisplayOptions
+export default DisplayOptions;
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,61 +1,72 @@
-import React, { Component } from 'react';
-import VerticalSidebar from './VerticalSidebar/VerticalSidebar'
-import Books from './Books/Books'
-import DisplayOptions from './DisplayOptions/DisplayOptions'
-import './Main.css'
-import bookService from '../services/bookService'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import VerticalSidebar from "./VerticalSidebar/VerticalSidebar";
+import Books from "./Books/Books";
+import DisplayOptions from "./DisplayOptions/DisplayOptions";
+import "./Main.css";
+import bookService from "../services/bookService";
+import { connect } from "react-redux";
 
 const mapDispatchToProps = dispatch => {
   return {
     onGetBooks: async function(books) {
       dispatch({
-        type: 'SET_BOOKS',
+        type: "SET_BOOKS",
         value: books
-      })
+      });
     }
-  }
-}
+  };
+};
 
-const DEFAULT_FILTER = { filterType : 'exists'}
+const DEFAULT_FILTER = { filterType: "exists" };
 const FAVE_FILTER = {
-  filterType: 'boolean',
-  filterKey: 'isFavorite', // TODO use a exported const for here and reducer
+  filterType: "boolean",
+  filterKey: "isFavorite", // TODO use a exported const for here and reducer
   filterValue: true
-}
+};
 const WISH_FILTER = {
-  filterType: 'boolean',
-  filterKey: 'isWishList', // TODO use a exported const for here and reducer
+  filterType: "boolean",
+  filterKey: "isWishList", // TODO use a exported const for here and reducer
   filterValue: true
-}
+};
 
+function createKVFilter(searchString) {
+  let [k, v] = searchString.split(":");
+  return {
+    filterType: "string",
+    filterKey: k,
+    filterValue: v
+  };
+}
+// todo : autosuggest keys?
 
 class Main extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      // viewFilter: {
-      //   filterType: 'boolean',
-      //   filterKey: 'isFavorite',
-      //   filterValue: true
-      // }
       viewFilter: DEFAULT_FILTER
+    };
+  }
+
+  setSearchFilter(searchString) {
+    if (searchString.length > 3 && searchString.includes(":")) {
+      let viewFilter = createKVFilter(searchString);
+      this.setState({ viewFilter });
     }
   }
 
   setFilterToFavorites() {
-    this.setState({ viewFilter: FAVE_FILTER })
+    this.setState({ viewFilter: FAVE_FILTER });
   }
   setFilterToWishList() {
-    this.setState({ viewFilter: WISH_FILTER })
+    this.setState({ viewFilter: WISH_FILTER });
   }
   setFilterToAll() {
-    this.setState({ viewFilter: DEFAULT_FILTER })
+    this.setState({ viewFilter: DEFAULT_FILTER });
   }
 
   async componentDidMount() {
-    const booksArray = await bookService.getBooks()
-    await this.props.onGetBooks(booksArray)
+    const booksArray = await bookService.getBooks();
+    await this.props.onGetBooks(booksArray);
   }
 
   render() {
@@ -72,14 +83,13 @@ class Main extends Component {
             setFilterToAll={this.setFilterToAll.bind(this)}
           />
           <div className="Main">
-            <DisplayOptions />
+            <DisplayOptions setSearchFilter={this.setSearchFilter.bind(this)} />
             <Books className="Books" viewFilter={this.state.viewFilter} />
           </div>
         </div>
-
       </div>
     );
   }
 }
 
-export default connect(null, mapDispatchToProps )(Main)
+export default connect(null, mapDispatchToProps)(Main);
