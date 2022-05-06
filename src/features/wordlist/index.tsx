2182eff26734cb842c37d6fd04563483ae5068f2
MagdaleneShaneLee <catsinahats@gmail.com>
2022-05-06 13:33:43 +0800

    slice for word

---
+++ src/features/wordlist/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+
+import styles from "./Wordlist.module.css";
+
+export default function Wordlist() {
+  const dispatch = useAppDispatch();
+  return <div>TODO</div>;
+}
---
+++ src/features/wordlist/wordlistSlice.ts
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { stat } from "fs";
+import { RootState, AppThunk } from "../../app/store";
+
+export interface WordlistState {
+  words: string[];
+}
+
+const initialState: WordlistState = {
+  words: [],
+};
+
+export const wordSlice = createSlice({
+  name: "words",
+  initialState,
+  reducers: {
+    addWord: (state, action: PayloadAction<string>) => {
+      state.words.push(action.payload);
+    },
+    ejectWord: (state, action: PayloadAction<number>) => {
+      state.words = [
+        ...state.words.slice(0, action.payload),
+        ...state.words.slice(action.payload + 1, state.words.length),
+      ];
+    },
+  },
+});
